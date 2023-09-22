73b402a85db76f8a02a204ee38a5bde27d9676f2
Will Epperson <willepp@live.com>
2023-09-22 01:37:55 -0400

    remove old code

--- textcleanui/src/App.svelte
+++ textcleanui/src/App.svelte
@@ -1,19 +1,5 @@
 <script lang="ts">
-  import type { DatabaseConnection } from "./database/loader";
-  import * as vg from "@uwdata/vgplot";
-  import DistSidebar from "./components/DistSidebar.svelte";
-  import InstanceView from "./components/InstanceView.svelte";
-
-  export let dbConnection: DatabaseConnection;
-
-  // export default vg.plot(
-  //   vg.line(vg.from("aapl"), { x: "Date", y: "Close" }),
-  //   vg.width(680),
-  //   vg.height(200)
-  // );
-
   let datasetName = "opus100_en_es";
-  // let dRef = dbConnection.loadDataset(datasetName);
 </script>
 
 <div class="bg-gradient-to-r from-blue-100 to-blue-700 dark:bg-sky-950 p-5">
@@ -23,35 +9,6 @@
   >
 </div>
 
-<!-- <div class="flex">
-  <div class="w-1/4">
-    <DistSidebar />
-  </div>
-  <div class="w-3/4">
-    <InstanceView />
-  </div>
-</div> -->
-
-<!-- <div>
-  {#await dRef}
-    <p>loading...</p>
-  {:then d}
-    <p>Loaded dataset: {datasetName}</p>
-    {vg.table({
-      from: datasetName,
-      columns: ["en", "es"],
-      width: { en: 200, es: 200 },
-      height: 500,
-    })}
-  {/await}
-</div> -->
-
 <div>
   <p>Loaded dataset: {datasetName}</p>
-  <!-- {vg.table({
-    from: datasetName,
-    columns: ["en", "es"],
-    width: { en: 200, es: 200 },
-    height: 500,
-  })} -->
 </div>
