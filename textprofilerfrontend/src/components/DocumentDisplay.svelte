838bbeca50a40bf9a809f1f020c717711bb0fa28
Will Epperson <willepp@live.com>
2024-02-01 04:17:56 -0500

    improved doc display styling

--- textprofilerfrontend/src/components/DocumentDisplay.svelte
+++ textprofilerfrontend/src/components/DocumentDisplay.svelte
@@ -12,44 +12,42 @@
 <div
   class={`rounded border bg-white ${highlight ? "border-dashed border-primary-500" : "border-solid border-gray-300"}`}
 >
-  <div class="flex items-center border-b border-gray-300 p-2">
+  <div class="flex items-center border-b border-gray-300 p-2 gap-1">
+    <button
+      class="bg-gray-50 hover:bg-gray-100 text-gray-500 p-1 rounded"
+      on:click={() => (toggle = !toggle)}
+    >
+      {#if toggle}
+        <AngleDownOutline size="xs" />
+      {:else}
+        <AngleRightOutline size="xs" />
+      {/if}
+    </button>
     <div>{docName}</div>
     <div class="grow"></div>
     <div class="text-gray-500">{id}</div>
   </div>
 
-  <div class="flex">
-    <!-- tool bar -->
-    <div class="flex flex-col gap-1 pt-2 pl-2">
-      <button
-        class="bg-gray-50 hover:bg-gray-100 text-gray-500 p-1 rounded"
-        on:click={() => (toggle = !toggle)}
-      >
-        {#if toggle}
-          <AngleDownOutline size="xs" />
-        {:else}
-          <AngleRightOutline size="xs" />
-        {/if}
-      </button>
-    </div>
-    <div
-      class={`overflow-auto p-2 w-full text-gray-800 ${toggle ? "max-h-96 " : "max-h-48 "}`}
-    >
+  <div class={`flex  ${toggle ? "max-h-none " : "max-h-48 "}`}>
+    <div class={` overflow-auto grow p-2 text-gray-800`}>
       {document}
     </div>
 
     {#if metadata.length}
-      <div class="bg-gray-50 p-2 min-w-48">
-        <table class="table-fixed">
-          <tbody>
-            {#each metadata as [itemKey, itemValue] (itemKey)}
-              <tr>
-                <td class="font-semibold italic">{itemKey}</td>
-                <td>{itemValue}</td>
-              </tr>
-            {/each}
-          </tbody>
-        </table>
+      <div class="overflow-auto shrink bg-gray-50 min-w-80">
+        {#each metadata as [itemKey, itemValue] (itemKey)}
+          <div class="flex border-b border-gray-200 px-2">
+            <div
+              class="whitespace-normal text-ellipsis overflow-hidden text-sm w-1/2 font-semibold italic"
+              title={itemKey}
+            >
+              {itemKey}
+            </div>
+            <div class="whitespace-normal overflow-scroll text-sm w-1/2">
+              {itemValue}
+            </div>
+          </div>
+        {/each}
       </div>
     {/if}
   </div>
