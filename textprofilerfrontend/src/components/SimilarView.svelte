5e836fdc38d339d6c88ed80ecebdb2a5a794e904
Will Epperson <willepp@live.com>
2024-02-01 03:43:32 -0500

    slight polish on view similar documents

--- textprofilerfrontend/src/components/SimilarView.svelte
+++ textprofilerfrontend/src/components/SimilarView.svelte
@@ -59,10 +59,6 @@
   $: dataPromise = getData(datasetInfo, similarDocID, firstTextColName);
 </script>
 
-<div class="flex items-center bg-gray-50 h-10 px-2">
-  <h3 class="self-center font-semibold text-lg">Similar View</h3>
-  <CloseButton on:click={clearFunc} />
-</div>
 <div class="max-h-screen overflow-auto">
   {#await dataPromise}
     <div>Loading...</div>
@@ -70,7 +66,12 @@
     <div
       class="p-4 sticky top-0 w-full bg-white border border-dashed border-primary-500"
     >
-      <h3 class="font-semibold mb-2">Pinned Document</h3>
+      <div class="flex items-center mb-2">
+        <h3 class="self-center font-semibold text-lg">
+          View similar documents
+        </h3>
+        <CloseButton on:click={clearFunc} />
+      </div>
 
       <DocumentDisplay
         id={data.originalDoc.id}
