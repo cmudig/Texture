f1a433b240745d97fa30fa38581c72005eaed158
Will Epperson <willepp@live.com>
2024-02-26 23:54:55 -0500

    Sync to mosaic (#53)
    * update deps so imports work
    
    * update Plot import

--- textprofilerfrontend/src/components/charts/chartUtils.ts
+++ textprofilerfrontend/src/components/charts/chartUtils.ts
@@ -1,4 +1,5 @@
 import * as vg from "@uwdata/vgplot";
+import { Plot } from "@uwdata/mosaic-plot";
 
 /**
  * Use instead of vg.plot to get the plot back instead of the element
@@ -6,7 +7,7 @@ import * as vg from "@uwdata/vgplot";
  * @returns vg.Plot()
  */
 export function getPlot(...directives) {
-  const p = new vg.Plot();
+  const p = new Plot();
   directives.flat().forEach((dir) => dir(p));
   p.marks.forEach((mark) => vg.coordinator().connect(mark));
   return p;
