eff9d5b0a51790e27a73aecdc70e9bfc052951a4
Will Epperson <willepp@live.com>
2024-03-20 17:59:48 -0400

    independent scrolling in table

--- textprofilerfrontend/src/components/table/RowView.svelte
+++ textprofilerfrontend/src/components/table/RowView.svelte
@@ -79,7 +79,7 @@
 </script>
 
 <div
-  class={`rounded border bg-white flex ${highlight ? "border-dashed border-primary-500" : "border-solid border-gray-300"}`}
+  class={`rounded border bg-white flex ${highlight ? "border-dashed border-primary-500" : "border-solid border-gray-300"} ${toggle ? "max-h-96" : "max-h-48"}`}
 >
   <div
     class="flex flex-col items-center border-r border-gray-300 p-1 gap-1 w-8 max-w-10 shrink-0"
@@ -99,79 +99,75 @@
   </div>
 
   <div
-    class={`flex  ${toggle ? "max-h-96 overflow-auto " : "max-h-48 overflow-hidden"}`}
+    class={`w-full px-2 pt-2 pb-4 text-gray-800 flex flex-col gap-1 border-gray-300 border-r ${toggle ? "overflow-auto" : "overflow-hidden"}`}
   >
-    <div class={`w-full px-2 pt-2 pb-4 text-gray-800 flex flex-col gap-1`}>
-      {#await wordSpans}
-        {#each textData as [textColName, textColData] (textColName)}
-          <div>
-            <div class="border-b border-gray-300 italic">
-              {textColName}
-              <Spinner class="ml-1" size="4" />
-            </div>
+    {#await wordSpans}
+      {#each textData as [textColName, textColData] (textColName)}
+        <div>
+          <div class="border-b border-gray-300 italic">
+            {textColName}
+            <Spinner class="ml-1" size="4" />
+          </div>
 
-            <div>
+          <div>
+            {formatValue(textColData, { type: "text" })}
+          </div>
+        </div>
+      {/each}
+    {:then wordSpanMap}
+      {#each textData as [textColName, textColData] (textColName)}
+        <div>
+          <div class="border-b border-gray-300 font-light text-secondary-700">
+            {textColName}
+          </div>
+
+          <!-- TODO: can be both, but right now only doing one at a time -->
+          {#if wordSpanMap && textColName in wordSpanMap}
+            <SpanIndexHighlight
+              highlights={wordSpanMap[textColName]}
+              value={textColData}
+            />
+          {:else if textColName in $selectionDisplay}
+            <SubstringHighlight
+              highlights={$selectionDisplay[textColName]}
+              value={textColData}
+            />
+          {:else}
+            <div class="text-gray-800">
               {formatValue(textColData, { type: "text" })}
             </div>
-          </div>
-        {/each}
-      {:then wordSpanMap}
-        {#each textData as [textColName, textColData] (textColName)}
-          <div>
-            <div class="border-b border-gray-300 font-light">
-              {textColName}
-            </div>
-
-            <!-- TODO: can be both, but right now only doing one at a time -->
-            {#if wordSpanMap && textColName in wordSpanMap}
-              <SpanIndexHighlight
-                highlights={wordSpanMap[textColName]}
-                value={textColData}
-              />
-            {:else if textColName in $selectionDisplay}
-              <SubstringHighlight
-                highlights={$selectionDisplay[textColName]}
-                value={textColData}
-              />
-            {:else}
-              <div class="text-gray-800">
-                {formatValue(textColData, { type: "text" })}
-              </div>
-            {/if}
-          </div>
-        {/each}
-      {/await}
-    </div>
-
-    {#if plotMetadata.length}
-      <div class=" border-gray-300 border-l w-80 shrink-0 h-full pt-3">
-        {#each plotMetadata as [itemKey, itemValue] (itemKey)}
-          {@const itemType = $datasetInfo?.columns.find(
-            (col) => col.name === itemKey,
-          )?.type}
-          <div class="flex px-2">
-            <div
-              class="whitespace-normal text-ellipsis overflow-hidden text-sm font-light text-secondary-700 w-1/3"
-              title={String(itemKey)}
-            >
-              {itemKey}
-            </div>
-            <div
-              class={`whitespace-normal break-words text-sm pl-1 w-2/3 ${itemValue == undefined ? "text-gray-300 italic" : "text-gray-700"}`}
-              class:bg-highlight-300={itemKey in $selectionDisplay &&
-                shouldHighlight(
-                  itemValue,
-                  $selectionDisplay[itemKey],
-                  itemType,
-                )}
-            >
-              <div>
-                {formatValue(itemValue, { type: itemType, colName: itemKey })}
-              </div>
-            </div>
-          </div>
-        {/each}
-      </div>
-    {/if}
+          {/if}
+        </div>
+      {/each}
+    {/await}
   </div>
+
+  {#if plotMetadata.length}
+    <div
+      class={`w-80 shrink-0 pt-2 ${toggle ? "overflow-auto" : "overflow-hidden"}`}
+    >
+      {#each plotMetadata as [itemKey, itemValue] (itemKey)}
+        {@const itemType = $datasetInfo?.columns.find(
+          (col) => col.name === itemKey,
+        )?.type}
+        <div class="flex px-2">
+          <div
+            class="whitespace-normal text-ellipsis overflow-hidden text-sm font-light text-secondary-700 w-1/3"
+            title={String(itemKey)}
+          >
+            {itemKey}
+          </div>
+          <div
+            class={`whitespace-normal break-words text-sm pl-1 w-2/3 ${itemValue == undefined ? "text-gray-300 italic" : "text-gray-800"}`}
+            class:bg-highlight-300={itemKey in $selectionDisplay &&
+              shouldHighlight(itemValue, $selectionDisplay[itemKey], itemType)}
+          >
+            <div>
+              {formatValue(itemValue, { type: itemType, colName: itemKey })}
+            </div>
+          </div>
+        </div>
+      {/each}
+    </div>
+  {/if}
 </div>
