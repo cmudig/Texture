bcc6cee91b1e20e984c1ab17dfc689b2647e45ac
Will Epperson <willepp@live.com>
2024-03-20 23:04:30 -0400

    format app spacing

--- textprofilerfrontend/src/App.svelte
+++ textprofilerfrontend/src/App.svelte
@@ -71,22 +71,22 @@
   }
 </script>
 
-<div
-  class="flex flex-row gap-2 bg-gradient-to-r from-primary-900 to-primary-500 p-4"
->
-  <span class="self-center whitespace-nowrap text-2xl text-white">
-    Text<span class="font-light ml-1"> Profiler</span>
-  </span>
-  <div class="grow" />
+<div class="h-screen flex flex-col">
+  <!-- Top bar -->
+  <div class="flex gap-2 bg-gradient-to-r from-primary-900 to-primary-500 p-4">
+    <span class="self-center whitespace-nowrap text-2xl text-white">
+      Text<span class="font-light ml-1"> Profiler</span>
+    </span>
+    <div class="grow" />
 
-  <Search
-    columnNames={$datasetInfo?.columns
-      .filter((col) => col.type === "text")
-      .map((col) => col.name)}
-    tableName={$datasetInfo?.name}
-  />
+    <Search
+      columnNames={$datasetInfo?.columns
+        .filter((col) => col.type === "text")
+        .map((col) => col.name)}
+      tableName={$datasetInfo?.name}
+    />
 
-  <!-- <FilePlusSolid
+    <!-- <FilePlusSolid
     id="addDatasetIcon"
     size="md"
     class="mx-1 self-center text-white hover:text-primary-700"
@@ -96,113 +96,109 @@
     >Add new dataset</Tooltip
   > -->
 
-  <AdjustmentsHorizontalOutline
-    id="settingsToggle"
-    size="md"
-    class="mx-1 self-center text-white hover:text-gray-300"
-  />
-  <Popover
-    triggeredBy="#settingsToggle"
-    class="z-10 w-80 bg-white text-sm font-light text-gray-500"
-    title="Settings"
-  >
-    <div class="flex flex-col gap-4 p-3">
-      <div class="flex flex-col gap-2 pb-2 border-b-2 border-grey-300">
-        <h3>Dataset</h3>
+    <AdjustmentsHorizontalOutline
+      id="settingsToggle"
+      size="md"
+      class="mx-1 self-center text-white hover:text-gray-300"
+    />
+    <Popover
+      triggeredBy="#settingsToggle"
+      class="z-10 w-80 bg-white text-sm font-light text-gray-500"
+      title="Settings"
+    >
+      <div class="flex flex-col gap-4 p-3">
+        <div class="flex flex-col gap-2 pb-2 border-b-2 border-grey-300">
+          <h3>Dataset</h3>
 
-        <Select
-          size="sm"
-          items={Object.values(datasets).map((k) => ({
-            value: k.name,
-            name: k.origin === "example" ? `${k.name} (example)` : k.name,
-          }))}
-          placeholder="Select dataset"
-          bind:value={currentDatasetName}
-          on:change={updateData}
-        />
+          <Select
+            size="sm"
+            items={Object.values(datasets).map((k) => ({
+              value: k.name,
+              name: k.origin === "example" ? `${k.name} (example)` : k.name,
+            }))}
+            placeholder="Select dataset"
+            bind:value={currentDatasetName}
+            on:change={updateData}
+          />
 
-        <SaveTableToFile />
-      </div>
-
-      <div class="flex flex-col gap-2 pb-2 border-b-2 border-grey-300">
-        <h3>Stopwords</h3>
-        <StopwordEditor />
-      </div>
-
-      <div class="flex flex-col gap-2">
-        <h3>Display Settings</h3>
-
-        <div>
-          <Label>Background distributions</Label>
-
-          <Toggle class="mt-2" bind:checked={$showBackgroundDist}
-            >Show in plot</Toggle
-          >
+          <SaveTableToFile />
         </div>
 
-        <div>
-          <Label>Display in table</Label>
-          <div class="mt-2 flex flex-col gap-1">
-            {#each $datasetInfo.columns as col}
-              <Toggle bind:checked={currentColToggleStates[col.name]}>
-                <span class="overflow-hidden text-ellipsis">
-                  {col.name}
-                </span>
-              </Toggle>
-            {/each}
+        <div class="flex flex-col gap-2 pb-2 border-b-2 border-grey-300">
+          <h3>Stopwords</h3>
+          <StopwordEditor />
+        </div>
+
+        <div class="flex flex-col gap-2">
+          <h3>Display Settings</h3>
+
+          <div>
+            <Label>Background distributions</Label>
+
+            <Toggle class="mt-2" bind:checked={$showBackgroundDist}
+              >Show in plot</Toggle
+            >
+          </div>
+
+          <div>
+            <Label>Display in table</Label>
+            <div class="mt-2 flex flex-col gap-1">
+              {#each $datasetInfo.columns as col}
+                <Toggle bind:checked={currentColToggleStates[col.name]}>
+                  <span class="overflow-hidden text-ellipsis">
+                    {col.name}
+                  </span>
+                </Toggle>
+              {/each}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </Popover>
+    </Popover>
 
-  <!-- <UploadDataModal
+    <!-- <UploadDataModal
     bind:panelOpen={showAddDataModal}
     finishedUploadHandler={(name) => {
       dataPromise = populateDataTables(name);
     }}
   /> -->
 
-  <ColumnTransformModal
-    bind:panelOpen={showAddColModal}
-    finishedCommitHandler={() => {
-      dataPromise = populateDataTables(currentDatasetName, false);
-    }}
-  />
-</div>
+    <ColumnTransformModal
+      bind:panelOpen={showAddColModal}
+      finishedCommitHandler={() => {
+        dataPromise = populateDataTables(currentDatasetName, false);
+      }}
+    />
+  </div>
 
-{#await dataPromise}
-  <div class="p-4">
-    <Spinner />
-  </div>
-{:then}
-  <div class="flex flex-row">
-    <div
-      class="h-screen w-[450px] shrink-0 overflow-scroll border-r border-gray-300"
-    >
-      <Sidebar {datasetColSummaries} />
+  <!-- Main content -->
+  {#await dataPromise}
+    <div class="p-4">
+      <Spinner />
     </div>
-    <div class="h-screen grow overflow-scroll pt-2 bg-gray-50">
-      {#if $compareSimilarID !== undefined}
-        <SimilarView
-          similarDocID={$compareSimilarID}
-          clearFunc={() => {
-            $compareSimilarID = undefined;
-          }}
-        />
-      {:else}
-        <DataDisplay {currentColToggleStates}>
-          <svelte:fragment slot="navBar">
-            <OptionsBar
-              bind:showAddColModal
-              {datasetSize}
-              {currentDatasetName}
-            />
-          </svelte:fragment>
-        </DataDisplay>
-      {/if}
+  {:then}
+    <div class="flex flex-1 overflow-hidden">
+      <div class="w-[450px] shrink-0 overflow-auto border-r border-gray-300">
+        <Sidebar {datasetColSummaries} />
+      </div>
+      <div class="flex-1 min-w-[450px] overflow-auto pt-2 bg-gray-50">
+        {#if $compareSimilarID !== undefined}
+          <SimilarView
+            similarDocID={$compareSimilarID}
+            clearFunc={() => {
+              $compareSimilarID = undefined;
+            }}
+          />
+        {:else}
+          <DataDisplay {currentColToggleStates}>
+            <svelte:fragment slot="navBar">
+              <OptionsBar bind:showAddColModal {datasetSize} />
+            </svelte:fragment>
+          </DataDisplay>
+        {/if}
+      </div>
     </div>
-  </div>
-{:catch error}
-  <div>Error: {error.message}</div>
-{/await}
+  {:catch error}
+    <div>Error: {error.message}</div>
+  {/await}
+</div>
