46208100623ff8bb2d6b4e1ec7e3faf55f2d03e6
Will Epperson <willepp@live.com>
2024-03-20 23:34:51 -0400

    remove spacing

--- textprofilerfrontend/src/components/OptionsBar.svelte
+++ textprofilerfrontend/src/components/OptionsBar.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import { Button } from "flowbite-svelte";
-  import { PlusSolid } from "flowbite-svelte-icons";
   import { formatNumber } from "../shared/format";
   import { filteredCount } from "../stores";
   import FilterBar from "./FilterBar.svelte";
@@ -8,7 +6,7 @@
   export let datasetSize;
 </script>
 
-<div class="grow px-2 self-center">
+<div class="grow self-center">
   <FilterBar />
 </div>
 <div class="text-md self-center">
