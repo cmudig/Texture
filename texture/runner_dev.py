79cc2ad6b2c3987b210a7755eb2fb43116d85fda
Will Epperson <willepp@live.com>
2024-05-02 19:28:52 -0400

    format runner dev

--- texture/runner_dev.py
+++ texture/runner_dev.py
@@ -3,7 +3,8 @@ from texture.runner import run
 
 def dev_run():
     """For launching server from poetry"""
-    run({"port": 8080})  # N.B frontend dev server hard coded for localhost:8080
+    # N.B frontend dev server hard coded for localhost:8080
+    run({})
 
 
 if __name__ == "__main__":
