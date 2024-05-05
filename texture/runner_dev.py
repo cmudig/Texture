2e7db3e5081880a27f08298fa18eac2ecbb152e7
Will Epperson <willepp@live.com>
2024-05-04 21:12:58 -0400

    working dev runner

--- texture/runner_dev.py
+++ texture/runner_dev.py
@@ -1,10 +1,24 @@
 from texture.runner import run
+import pandas as pd
 
 
 def dev_run():
     """For launching server from poetry"""
     # N.B frontend dev server hard coded for localhost:8080
-    run({})
+    df = pd.DataFrame(
+        {
+            "id": [1, 2, 3],
+            "text": [
+                "Hello world how are you?",
+                "I'm fine how about you?",
+                "The weather is great today.",
+            ],
+            "label": [0, 1, 0],
+            "umap_x": [0.1, 0.2, 0.3],
+            "umap_y": [0.4, 0.5, 0.6],
+        }
+    )
+    run(df, load_example_data=False)
 
 
 if __name__ == "__main__":
