07281c593509cf1447dff250bc0403973cc39b5b
Will Epperson <willepp@live.com>
2024-05-07 17:14:34 -0400

    use multiprocess instead of multiprocessing and safer input checks before process split

--- texture/runner.py
+++ texture/runner.py
@@ -1,18 +1,24 @@
 import uvicorn
-from typing import Dict, Union, Any, List
-from multiprocessing import Process
+from typing import Dict, Any, List
+import multiprocess
 import pandas as pd
 
 from texture.models import (
     TextureInitArgs,
     DatasetInitArgs,
     ColumnInputInfo,
+    DatasetInfo,
 )
 from texture.server import get_server
 from texture.utils import is_notebook
 from texture import preprocess
 
 TEXTURE_SERVER_PROCESS = None
+
+try:
+    multiprocess.set_start_method("spawn", force=True)
+except RuntimeError as e:
+    pass
 
 
 def run(
@@ -38,6 +44,8 @@ def run(
         api_key=api_key,
     )
 
+    dsInfo, load_tables, load_embeddings = preprocess.validate_and_run_preprocess(args)
+
     if is_notebook():
         print("Running from a notebook, starting a new process")
 
@@ -46,25 +54,29 @@ def run(
             print("Terminating existing server process")
             TEXTURE_SERVER_PROCESS.terminate()
 
-        TEXTURE_SERVER_PROCESS = Process(
+        TEXTURE_SERVER_PROCESS = multiprocess.Process(
             target=run_server,
-            args=(args,),
+            args=(args, dsInfo, load_tables, load_embeddings),
         )
         TEXTURE_SERVER_PROCESS.start()
         # below will block notebook from progressing past server cell
         # TEXTURE_SERVER_PROCESS.join()
     else:
-        run_server(args)
+        run_server(args, dsInfo, load_tables, load_embeddings)
 
 
-def run_server(args: Union[TextureInitArgs, Dict]):
-    args = TextureInitArgs(**args) if isinstance(args, dict) else args
-
-    dsInfo, load_tables, load_embeddings = preprocess.validate_and_run_preprocess(args)
+def run_server(
+    args: TextureInitArgs,
+    dsInfo: DatasetInfo,
+    load_tables: Dict,
+    load_embeddings: Dict | None,
+):
 
     app = get_server(
         DatasetInitArgs(
-            datasetInfo=dsInfo, load_tables=load_tables, load_embeddings=load_embeddings
+            datasetInfo=dsInfo,
+            load_tables=load_tables,
+            load_embeddings=load_embeddings,
         ),
         load_example_data=args.load_example_data,
         api_key=args.api_key,
@@ -75,6 +87,6 @@ def run_server(args: Union[TextureInitArgs, Dict]):
         app,
         host=args.host,
         port=args.port,
-        log_level="warning",
+        log_level="warning",  # info or warning
         # reload=True,
     )
