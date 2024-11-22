e2b4c36f53b26eab9aaa8a106e184d5e7b028b3c
Will Epperson <willepp@live.com>
2024-11-22 04:11:36 -0500

    settings tweaks

--- texturefrontend/src/components/settings/SettingsPanel.svelte
+++ texturefrontend/src/components/settings/SettingsPanel.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
-  import { Select, Toggle, Label, Button } from "flowbite-svelte";
+  import { Select, Label } from "flowbite-svelte";
   import type { DatasetSchema } from "../../backendapi";
 
-  import { datasetSchema, showBackgroundDistMap } from "../../stores";
+  import { showBackgroundDistMap, showSegmentValues } from "../../stores";
   import SaveTableToFile from "../SaveTableToFile.svelte";
 
   export let datasets: Record<string, DatasetSchema>;
   export let currentDatasetName: string;
   export let updateData: () => void;
-  export let currentColToggleStates: Record<string, boolean>;
   export let allowDeriveNew: boolean;
 
   function setBackgroundDist(val: boolean) {
@@ -22,26 +21,62 @@
   }
 </script>
 
-<div class="flex flex-col gap-4 p-3">
-  <div class="flex flex-col gap-2 pb-2 border-b-2 border-grey-300">
-    <h3>Dataset</h3>
+<div class="flex flex-col gap-4 p-2">
+  <div class="flex flex-col gap-2">
+    <h3 class="text-base">Dataset</h3>
 
-    <Select
-      size="sm"
-      items={Object.values(datasets).map((k) => ({
-        value: k.name,
-        name: k.origin === "example" ? `${k.name} (example)` : k.name,
-      }))}
-      placeholder="Select dataset"
-      bind:value={currentDatasetName}
-      on:change={updateData}
-    />
+    <div class="flex gap-2 items-center">
+      <Select
+        size="sm"
+        items={Object.values(datasets).map((k) => ({
+          value: k.name,
+          name: k.origin === "example" ? `${k.name} (example)` : k.name,
+        }))}
+        placeholder="Select dataset"
+        bind:value={currentDatasetName}
+        on:change={updateData}
+      />
 
-    <SaveTableToFile />
+      <SaveTableToFile />
+    </div>
   </div>
 
+  <hr />
+
   <div class="flex flex-col gap-2">
-    <h3>Display Settings</h3>
+    <h3 class="text-base">Display Settings</h3>
+
+    <div class="flex gap-2 items-center">
+      <input
+        type="checkbox"
+        class="w-4 h-4 bg-gray-100 border-gray-300 dark:ring-offset-gray-800 focus:ring-2 me-2 dark:bg-gray-700 dark:border-gray-600 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600"
+        bind:checked={$showSegmentValues}
+      />
+
+      <Label>Show segment values in table</Label>
+    </div>
+
+    <div class="flex gap-2 items-center">
+      <button
+        class="hover:bg-gray-100 bg-gray-50 rounded border px-2 border-gray-200"
+        on:click={() => setBackgroundDist(true)}
+      >
+        On
+      </button>
+      <button
+        class="hover:bg-gray-100 bg-gray-50 rounded border px-2 border-gray-200"
+        on:click={() => setBackgroundDist(false)}
+      >
+        Off
+      </button>
+      <Label>Background distributions</Label>
+    </div>
+  </div>
+
+  <hr />
+
+  <div class="flex flex-col gap-2">
+    <h3 class="text-base">Feature Flags</h3>
 
     <div class="flex gap-2 items-center">
       <input
@@ -51,28 +86,6 @@
       />
 
       <Label>Allow derive new</Label>
-    </div>
-
-    <div>
-      <Label>Background distributions</Label>
-
-      <Button on:click={() => setBackgroundDist(true)} size="sm">All on</Button>
-      <Button on:click={() => setBackgroundDist(false)} size="sm">
-        All off
-      </Button>
-    </div>
-
-    <div>
-      <Label>Display in table</Label>
-      <div class="mt-2 flex flex-col gap-1">
-        {#each $datasetSchema.columns as col}
-          <Toggle bind:checked={currentColToggleStates[col.name]}>
-            <span class="overflow-hidden text-ellipsis">
-              {col.name}
-            </span>
-          </Toggle>
-        {/each}
-      </div>
     </div>
   </div>
 </div>
