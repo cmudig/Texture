1db06fb59c69732d378fb61eddacfc1b722c8161
Will Epperson <willepp@live.com>
2024-11-23 18:46:50 -0500

    use toggle component for sidebar

---
+++ texturefrontend/src/components/Toggle.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let active = true;
+</script>
+
+<div>
+  <button
+    class="space-between flex gap-2 h-9 w-full items-center justify-between px-2 hover:bg-gray-200 border-t-2 border-secondary-200"
+    on:click={() => {
+      active = !active;
+    }}
+  >
+    <slot name="title" />
+  </button>
+
+  <div class="w-full pl-4 pr-2 mb-2" class:hidden={!active}>
+    <slot name="body" />
+  </div>
+</div>
